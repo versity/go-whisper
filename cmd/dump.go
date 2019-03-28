443e6b9d4f86dbd48e7c901493451a92b8c529ac
bom-d-van <bom.d.van@gmail.com>
2019-03-28 13:55:40 +0100

    cmd/dump.go: add less mode (on by default)

--- cmd/dump.go
+++ cmd/dump.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"flag"
+	"io/ioutil"
 	"os"
+	"os/exec"
 
 	whisper "github.com/go-graphite/go-whisper"
 )
 
 func main() {
-	db, err := whisper.OpenWithOptions(os.Args[1], &whisper.Options{})
+	noLess := flag.Bool("no-less", false, "Don't use less, print everything to stdout.")
+	flag.Parse()
+
+	less := exec.Command("less")
+	if !*noLess {
+		less.Stdout = os.Stdout
+		temp, err := ioutil.TempFile("", "")
+		if err != nil {
+			panic(err)
+		}
+		os.Stdout = temp
+	}
+
+	db, err := whisper.OpenWithOptions(flag.Args()[0], &whisper.Options{})
 	if err != nil {
 		panic(err)
 	}
-
 	db.Dump(true)
+
+	if !*noLess {
+		if _, err := os.Stdout.Seek(0, 0); err != nil {
+			panic(err)
+		}
+		less.Stderr = os.Stderr
+		less.Stdin = os.Stdout
+		if err := less.Run(); err != nil {
+			panic(err)
+		}
+	}
 }
