a8ae514f75491b74019ed6e5121c59b32b15cf49
bom-d-van <bom.d.van@gmail.com>
2019-04-01 14:15:46 +0200

    cmd/icheck.go: remove outdated codes

--- cmd/icheck.go
+++ cmd/icheck.go
@@ -5,20 +5,13 @@ import (
 	"os"
 
 	whisper "github.com/go-graphite/go-whisper"
-	"github.com/kr/pretty"
 )
-
-var _ = pretty.Println
 
 func init() {
 	log.SetFlags(log.Lshortfile)
 }
 
 func main() {
-	// whisper.Now = func() time.Time {
-	// 	return time.Unix(1544478230, 0)
-	// }
-
 	file1 := os.Args[1]
 
 	db1, err := whisper.OpenWithOptions(file1, &whisper.Options{})
