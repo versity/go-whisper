720ab5a91dbd3befb67457ad894cd5a7a160b1ee
xiaofan hu <bom.d.van@gmail.com>
2021-05-12 15:30:57 +0200

    cmd/read: actually implement the read cmd
    The original cmd/read.go was committed by mistake and it wasn't implemented.
    This commit fixes that. The difference between it and cmd/dump.go is that it reads
    data using the Fetch api, instead of fetching the raw data directly from whisper
    file. In a way, it is similar to reading data with render api in go-carbon.
    
    An example usage:
        ./read -quiet -from '2021-05-12 08:11:21' -until '2021-05-12 08:12:22'

--- cmd/read.go
+++ cmd/read.go
@@ -3,45 +3,135 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
-	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	whisper "github.com/go-graphite/go-whisper"
 )
 
 func main() {
-	var body string
-	if len(os.Args) < 2 {
-		fmt.Println("write: write data points to a whisper file.\nwrite file.wsp [1572940800:3,1572940801:5]\n")
-		os.Exit(1)
-	} else if len(os.Args) > 2 {
-		body = os.Args[2]
-	} else {
-		in, err := ioutil.ReadAll(os.Stdin)
-		if err != nil {
-			panic(err)
-		}
-		body = string(in)
-	}
+	archive := flag.Int("archive", -1, "read data from specified archive (0-indexed).")
+	from := flag.String("from", "", "read data from the specified timestamp.")
+	until := flag.String("until", "", "read data until the specified timestamp.")
+	format := flag.String("format", "txt", "specify the format of the data returned: json, txt.")
+	archiveNum := flag.Bool("archive-num", false, "check the number of archives")
+	timezone := flag.String("timezone", "Local", `timezone to parse from/until timestamps: Local, UTC, or a location name corresponding to a file in the IANA Time Zone database, such as "America/New_York". https://golang.org/pkg/time/#LoadLocation.`)
+	help := flag.Bool("help", false, "print help messages.")
+	keepNaN := flag.Bool("nan", false, "Show NaN/absent data points. If false it will be returned as 0.")
+	quiet := flag.Bool("quiet", false, "print only data, no other messages.")
+	flag.BoolVar(help, "h", false, "print help messages.")
 
-	db, err := whisper.OpenWithOptions(os.Args[1], &whisper.Options{FLock: true})
+	flag.Parse()
+
+	location, err := time.LoadLocation(*timezone)
 	if err != nil {
 		panic(err)
 	}
 
-	if err := db.UpdateMany(parse(body)); err != nil {
-		panic(err)
+	if len(flag.Args()) == 0 || *help {
+		fmt.Printf(
+			"usage: read -from %q -until %q whisper_filename\n",
+			time.Now().In(location).Add(-4*time.Hour).Format("2006-01-02 15:03:06"),
+			time.Now().In(location).Add(-5*time.Minute).Format("2006-01-02 15:03:06"))
+		flag.PrintDefaults()
+		os.Exit(0)
 	}
 
-	if err := db.Close(); err != nil {
+	openOption := os.O_RDONLY
+	db, err := whisper.OpenWithOptions(flag.Arg(0), &whisper.Options{FLock: true, OpenFileFlag: &openOption})
+	if err != nil {
 		panic(err)
 	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			panic(err)
+		}
+	}()
 
-	if db.Extended {
-		fmt.Println("file has been extended.")
+	if *archiveNum {
+		fmt.Println(len(db.Retentions()))
+		os.Exit(0)
+	}
+
+	if *archive == -1 && (*from == "" || *until == "") {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	var fromTime, untilTime int
+	if *archive != -1 {
+		rets := db.Retentions()
+		if *archive >= len(rets) {
+			fmt.Fprintf(os.Stderr, "The whisper file only has %d archives.\n", len(rets))
+			os.Exit(2)
+		}
+
+		if *until == "" {
+			untilTime = int(time.Now().Unix())
+		} else {
+			t, err := time.ParseInLocation("2006-01-02 15:04:05", *until, location)
+			if err != nil {
+				panic(err)
+			}
+			untilTime = int(t.Unix())
+		}
+
+		fromTime = untilTime - rets[*archive].MaxRetention()
+	} else {
+		t1, err := time.ParseInLocation("2006-01-02 15:04:05", *from, location)
+		if err != nil {
+			panic(err)
+		}
+		fromTime = int(t1.Unix())
+
+		t2, err := time.ParseInLocation("2006-01-02 15:04:05", *until, location)
+		if err != nil {
+			panic(err)
+		}
+		untilTime = int(t2.Unix())
+	}
+
+	if !*quiet {
+		fmt.Printf("reading data from %d (%s) to %d (%s)\n", fromTime, time.Unix(int64(fromTime), 0), untilTime, time.Unix(int64(untilTime), 0))
+	}
+
+	data, err := db.Fetch(fromTime, untilTime)
+	if err != nil {
+		panic(err)
+	} else if data == nil {
+		fmt.Println("No data is fetched. Bad timerange (from/until)?")
+		return
+	}
+
+	var ps []whisper.TimeSeriesPoint
+	for _, p := range data.Points() {
+		if math.IsNaN(p.Value) {
+			if !*keepNaN {
+				continue
+			}
+			p.Value = 0
+		}
+
+		ps = append(ps, p)
+	}
+
+	switch *format {
+	case "json":
+		bytes, err := json.Marshal(ps)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(bytes))
+	default:
+		for _, p := range ps {
+			fmt.Printf("%d: %v\n", p.Time, p.Value)
+		}
 	}
 }
 
